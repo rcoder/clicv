f4cb7cc2492a44b927424acb305fd2dc57ad523d
Lennon Day-Reynolds <lennon@rcoder.net>
2019-10-02 14:45:21 -0700

    use prettier

--- main.ts
+++ main.ts
@@ -21,25 +21,18 @@ inquirer.registerPrompt('suggest', require('inquirer-prompt-suggest'));
 
 const sessionId = `${new Date().getTime()}-${randomBytes(4).toString('hex')}`;
 
-const injectSession = format((info, _) =>
-  ({ ...info, session: sessionId})
-);
+const injectSession = format((info, _) => ({ ...info, session: sessionId }));
 
 const log = createLogger({
-  transports: [
-    new transports.File({ filename: 'clicv.log'})
-  ],
-  format: format.combine(
-    format.json(),
-    injectSession()
-  )
+  transports: [new transports.File({ filename: 'clicv.log' })],
+  format: format.combine(format.json(), injectSession())
 });
 
 type Page = {
   title: string;
   name: string;
   content: string;
-}
+};
 
 const pagesDir = path.join(__dirname, 'pages');
 
@@ -50,9 +43,10 @@ const smtpParams = {
 
 const sendTo = 'lennon+jobinfo@bc8.org';
 
-const defaultEditorMessage = '\n\n# Enter your message above. '
-  + 'Type Ctrl-D to save and finish, or Ctrl-C to cancel.\n'
-  + '# These lines will be removed automatically.';
+const defaultEditorMessage =
+  '\n\n# Enter your message above. ' +
+  'Type Ctrl-D to save and finish, or Ctrl-C to cancel.\n' +
+  '# These lines will be removed automatically.';
 
 const mail = mailer.createTransport(smtpParams);
 
@@ -63,7 +57,7 @@ const loadPage = (name: string) => {
   } else {
     throw new Error(`couldn't find page ${name}`);
   }
-}
+};
 
 const screenBufOptions = {
   attr: {
@@ -96,16 +90,16 @@ log.info(`found ${allPages.length} pages`, {
   pages: allPages.map(page => page.name)
 });
 
-allPages.forEach(page => 
-  cli.command(page.name, chalk.greenBright(page.title))
-    .action(async () => {
-      log.info(`Loading page ${page.name}`);
-      renderPage(page.name)
-    })
+allPages.forEach(page =>
+  cli.command(page.name, chalk.greenBright(page.title)).action(async () => {
+    log.info(`Loading page ${page.name}`);
+    renderPage(page.name);
+  })
 );
 
-cli.command('contact', chalk.yellow('Send email to Lennon'))
-  .action(async (args) => {
+cli
+  .command('contact', chalk.yellow('Send email to Lennon'))
+  .action(async args => {
     log.info('Starting message editor');
 
     cli.hide();
@@ -115,7 +109,8 @@ cli.command('contact', chalk.yellow('Send email to Lennon'))
         type: 'input',
         name: 'from',
         message: 'From:',
-        validate: input => input.match(emailPat) ? true : 'please enter an email address'
+        validate: input =>
+          input.match(emailPat) ? true : 'please enter an email address'
       },
       {
         type: 'suggest',
@@ -126,8 +121,9 @@ cli.command('contact', chalk.yellow('Send email to Lennon'))
           'Job Opening',
           'Introduction',
           'Top Urgent!'
-        ].sort((a, b) => Math.random()-0.5),
-        validate: input => input.match(/\w+/) ? true : 'please enter a subject'
+        ].sort((a, b) => Math.random() - 0.5),
+        validate: input =>
+          input.match(/\w+/) ? true : 'please enter a subject'
       },
       {
         type: 'suggest',
@@ -138,7 +134,8 @@ cli.command('contact', chalk.yellow('Send email to Lennon'))
           'Phone (provided in message)',
           'Other (described in message)'
         ],
-        validate: input => input.match(/\w+/) ? true : 'please enter a contact method'
+        validate: input =>
+          input.match(/\w+/) ? true : 'please enter a contact method'
       }
     ]);
 
@@ -193,4 +190,4 @@ cli.command('contact', chalk.yellow('Send email to Lennon'))
 cli.exec('help');
 cli.delimiter(defaultPrompt).show();
 
-log.info(`exiting`);
+log.info('exiting');
